fd1b773ddf3aa568fac30f3936334e7f2a4b4535
Jiahui Wang <wjh4289042@hotmail.com>
2020-01-28 17:21:34 -0800

    start api

---
+++ routes/api.js
@@ -0,0 +1,22 @@
+const router = require("express").Router(); //what's Router()
+const Workout = require("../models/workout.js");
+
+router.post("/api/workout", ({ body }, res) => {
+  Workout.create(body)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.post("/api/workout/bulk", ({ body }, res) => {
+  Workout.insertMany(body)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
---
+++ server.js
@@ -0,0 +1,31 @@
+// Server.js - This file is the initial starting point for the Node/Express server.
+// *** Dependencies
+const express = require("express");
+const mongoose = requrie("mongoose");
+
+// Sets up the Express App
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Static directory
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+// Requiring our models for syncing
+const db = require("./models");
+
+// Routes
+require("./routes/api-routes.js")(app);
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+  });
+  
