64ecde0b86d3f49a9c563b1cfc88d5c9e1a75da8
Jiahui Wang <wjh4289042@hotmail.com>
2020-01-30 09:59:44 -0800

    add dotenv

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 // *** Dependencies
 const express = require("express");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
 // Sets up the Express App
 const app = express();
