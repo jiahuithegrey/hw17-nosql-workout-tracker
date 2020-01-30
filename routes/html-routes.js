64ca9c7c57085de49deb2b0374ac4c029ad78552
Jiahui Wang <wjh4289042@hotmail.com>
2020-01-30 09:30:14 -0800

    use _ _ for dirname

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,15 +3,21 @@ const path = require("path");
 const router = require("express").Router();
 
   router.get("/", (req, res) => {
-    res.sendFile(path.join(_dirname, "../public/index.html"));
+    res.sendFile(path.join(__dirname, "../public/index.html"), err =>{
+        if (err) throw err;
+    });
   });
 
   router.get("/exercise", (req, res) => {
-    res.sendFile(path.join(_dirname, "../public/exercise.html"));
+    res.sendFile(path.join(__dirname, "../public/exercise.html"), err =>{
+        if (err) throw err;
+    });
   });
 
   router.get("/stats", (req, res) => {
-    res.sendFile(path.join(_dirname, "../public/stats.html"));
+    res.sendFile(path.join(__dirname, "../public/stats.html"), err =>{
+        if (err) throw err;
+    });
   });
 
   //is this right?
